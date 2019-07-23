589016f8cac0272ad7bbeded2b41da6931a9335e
MangKkung <47519566+Dan-IoT-2019-Summer@users.noreply.github.com>
2019-07-23 15:47:41 -0700

    2019_07_23 android design update

--- main/java/com/example/design/MainActivity.java
+++ main/java/com/example/design/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.design;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
